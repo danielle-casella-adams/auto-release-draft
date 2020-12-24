06d44ba6e361736729a9d9ed56b70b7e4bc36b6e
Danielle Adams <danielle.adams3@extendhealth.com>
2020-12-24 11:07:30 -0700

    Adding semver and event info.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import * as core from '@actions/core'
+import * as event from './event.ts'
+import * as version from './version'
 
 export async function run(): Promise<void> {
   try {
-    core.setOutput('release-url', "https://example.com")
+    const tag = event.getCreatedTag();
+    if (tag && version.isSemVer(tag)) {
+      //TODO:
+    }
+
+    core.setOutput('release-url', 'https://example.com');
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
